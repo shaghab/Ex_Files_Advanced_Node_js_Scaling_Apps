e53f20e54095bc8afbccd522c4dd3e8c00500d53
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-04-02 17:01:41 +0500

    dynamically creating clones based on number of CPUs

--- Exercise Files/ch01/01_04/start/index.js
+++ Exercise Files/ch01/01_04/start/index.js
@@ -1,11 +1,18 @@
+const http = require("http");
 const cluster = require("cluster");
 const numCPUs = require("os").cpus().length;
 
 if (cluster.isMaster) {
   console.log("This is the master process", process.pid);
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 } else {
-  console.log("this is the worker process", process.pid);
+  http
+    .createServer((req, res) => {
+      const message = `worked ${process.pid}...`;
+      console.log(message);
+      res.end(message);
+    })
+    .listen(3000);
 }
