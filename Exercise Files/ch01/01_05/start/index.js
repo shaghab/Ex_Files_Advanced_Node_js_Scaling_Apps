9c1638a0caf6e277f46cab72f6854cc3aede7105
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-04-03 05:12:03 +0500

    logging starting and killing of worker procces

--- Exercise Files/ch01/01_05/start/index.js
+++ Exercise Files/ch01/01_05/start/index.js
@@ -7,7 +7,13 @@ if (cluster.isMaster) {
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
+
+  cluster.on("exit", (worker) => {
+    console.log(`worker process ${process.pid} has died`);
+    console.log((`only ${Object.keys(cluster.workers}).length`));
+  });
 } else {
+  console.log(`started a worker at ${process.pid}`);
   http
     .createServer((req, res) => {
       const message = `worker ${process.pid}...`;
