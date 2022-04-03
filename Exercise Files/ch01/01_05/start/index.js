b824269b68c8e3372a9994decd46c3decf9cd61b
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-04-03 05:08:23 +0500

    formatted

--- Exercise Files/ch01/01_05/start/index.js
+++ Exercise Files/ch01/01_05/start/index.js
@@ -1,16 +1,18 @@
-const http = require('http')
-const cluster = require('cluster')
-const numCPUs = require('os').cpus().length
+const http = require("http");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
 if (cluster.isMaster) {
-  console.log('this is the master process: ', process.pid)
-  for (let i=0; i<numCPUs; i++) {
-    cluster.fork()
+  console.log("this is the master process: ", process.pid);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
 } else {
-  http.createServer((req, res) => {
-    const message = `worker ${process.pid}...`
-    console.log(message)
-    res.end(message)
-  }).listen(3000)
+  http
+    .createServer((req, res) => {
+      const message = `worker ${process.pid}...`;
+      console.log(message);
+      res.end(message);
+    })
+    .listen(3000);
 }
