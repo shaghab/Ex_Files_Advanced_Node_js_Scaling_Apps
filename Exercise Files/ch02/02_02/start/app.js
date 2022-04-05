4f4c7e315ba8a5c06d04ec1f00a7ca0e41cb96f5
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-04-05 22:51:34 +0500

    formatted

--- Exercise Files/ch02/02_02/start/app.js
+++ Exercise Files/ch02/02_02/start/app.js
@@ -1,14 +1,14 @@
-const http = require('http')
+const http = require("http");
 
-let requests = 0
+let requests = 0;
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        requests++
-        console.log(`${process.pid}: ${requests}`)
-        res.end(JSON.stringify(requests))
-    }
-})
+  if (req.url === "/") {
+    requests++;
+    console.log(`${process.pid}: ${requests}`);
+    res.end(JSON.stringify(requests));
+  }
+});
 
-server.listen(3000)
-console.log(`counting requests`)
+server.listen(3000);
+console.log(`counting requests`);
