fb2aaba31bfe29c30f7dbcfe4c326a1ea98bd6da
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-04-07 05:26:47 +0500

    formatted

--- Exercise Files/ch02/02_04/start/db.js
+++ Exercise Files/ch02/02_04/start/db.js
@@ -1,31 +1,30 @@
-const { LocalStorage } = require('node-localstorage')
+const { LocalStorage } = require("node-localstorage");
 
-const db = new LocalStorage('data')
+const db = new LocalStorage("data");
 
-const loadCats = () => JSON.parse(db.getItem("cats") || '[]')
+const loadCats = () => JSON.parse(db.getItem("cats") || "[]");
 
-const hasCat = name => loadCats()
-    .map(cat => cat.name)
-    .includes(name)
+const hasCat = (name) =>
+  loadCats()
+    .map((cat) => cat.name)
+    .includes(name);
 
 module.exports = {
-
-    addCat(newCat) {
-        if (!hasCat(newCat.name)) {
-            let cats = loadCats()
-            cats.push(newCat)
-            db.setItem("cats", JSON.stringify(cats, null, 2))
-        }
-    },
-
-    findCatByName(name) {
-        let cats = loadCats()
-        return cats.find(cat => cat.name === name)
-    },
-
-    findCatsByColor(color) {
-        let cats = loadCats()
-        return cats.filter(cat => cat.color === color)
+  addCat(newCat) {
+    if (!hasCat(newCat.name)) {
+      let cats = loadCats();
+      cats.push(newCat);
+      db.setItem("cats", JSON.stringify(cats, null, 2));
     }
+  },
 
-}
+  findCatByName(name) {
+    let cats = loadCats();
+    return cats.find((cat) => cat.name === name);
+  },
+
+  findCatsByColor(color) {
+    let cats = loadCats();
+    return cats.filter((cat) => cat.color === color);
+  },
+};
