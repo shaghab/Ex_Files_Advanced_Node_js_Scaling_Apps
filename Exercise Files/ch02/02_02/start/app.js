6285dac38ae288276d6d1dfa404784c0360d8865
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-04-07 05:26:20 +0500

    taken request counter into a file based DB

--- Exercise Files/ch02/02_02/start/app.js
+++ Exercise Files/ch02/02_02/start/app.js
@@ -1,10 +1,15 @@
 const http = require("http");
 
+const { LocalStorage } = require("node-localstorage");
+
+const db = new LocalStorage("./data");
+
 let requests = 0;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    requests++;
+    let requests = db.getItem("requests");
+    db.setItem("requests", ++requests);
     console.log(`${process.pid}: ${requests}`);
     res.end(JSON.stringify(requests));
   }
